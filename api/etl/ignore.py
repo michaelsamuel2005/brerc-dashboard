2455fefb698e12c0f0832c89268c1dc4c0cb248c
Kariizi <tingting.hv@hotmail.com>
2026-07-21 18:43:25 +0100

    removed ignore.py

---
+++ api/etl/ignore.py
@@ -0,0 +1,129 @@
+import pandas as pd
+
+def clean_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Standardise column names.
+    """
+
+    df = df.copy()
+
+    df.columns = (df.columns
+        # Removes whitespace, lowercases + adding underscores
+        .str.strip()
+        .str.lower()
+        .str.replace(" ", "_")
+    )
+
+    return df
+
+def validate_dates(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function to check if theres any rows where both dates are missing
+    """
+
+    invalid_rows = df[
+        df["precise_date"].isna() &
+        df["vague_date"].isna()
+    ]
+
+    return invalid_rows
+
+def duplicate_data(df: pd.DataFrame) -> None:
+    print("\n===== DUPLICATE VALUES PER COLUMN =====")
+
+    for column in df.columns:
+        duplicate_count = df[column].dropna().duplicated().sum()
+
+        print(f"{column}: {duplicate_count} duplicate values")
+
+def duplicate_species(df: pd.DataFrame) -> None:
+    column = "scientific_name"
+
+    print(f"\n===== DUPLICATES IN {column} =====")
+
+    duplicate_values = (
+        # Selects given column, counts the amount of times each value appears, keep values where count is greater than 1
+        df[column]
+        .value_counts()
+        .loc[lambda x: x > 1]
+    )
+    
+    pd.set_option("display.max_rows", None)
+    print(duplicate_values)
+
+def total_species(df: pd.DataFrame) -> None:
+
+    # converting obj -> num for summing
+    df["abundance"] = pd.to_numeric(
+        df["abundance"],
+        errors="coerce"
+    )
+
+    species_totals = (
+        # Groups rows with same value in same group (looks at values in column, creates group for every unique value)
+        # For each group, look at abundance
+        # Sum together values in abundance
+        df.groupby("scientific_name")["abundance"]
+        .sum()
+    )
+
+    print("\n===== TOTAL COUNT PER SPECIES =====")
+    print(species_totals)
+
+# Takes in df (dataset) which is pandas and returns pandas dataframe (run via python test_cleaning.py)
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Clean and standardise dataset.
+    """
+
+    # Make a copy so we don't accidentally modify the original dataframe
+    df = df.copy()
+
+    print("===== FIRST 5 ROWS =====")
+    print(df.head())
+
+    print("\n===== DATASET SHAPE =====")
+    print(df.shape)
+
+    print("\n===== COLUMN NAMES BEFORE CLEANING =====")
+    print(df.columns.tolist())
+
+    # Actual cleaning
+    df = clean_column_names(df)
+
+    print("\n===== COLUMN NAMES AFTER CLEANING =====")
+    print(df.columns.tolist())
+
+    print("\n===== DATA TYPES AND MISSING VALUES =====")
+    df.info()
+
+    print("\n===== MISSING VALUES =====")
+    print(df.isna().sum())
+
+    print("\n===== MISSING PERCENTAGE =====")
+    print((df.isna().mean() * 100).round(2))
+
+    print("\n===== DUPLICATE ROWS =====")
+    print(df.duplicated().sum())
+
+    print("\n===== DATA TYPES =====")
+    print(df.dtypes)
+
+    print("\n===== DATE VALIDATION =====")
+
+    print(
+        df[["precise_date", "vague_date"]]
+        .isna()
+        .value_counts()
+    )
+
+    invalid_dates = validate_dates(df)
+
+    print("\nRows with both dates missing:")
+    print(len(invalid_dates))
+
+    duplicate_data(df)
+    duplicate_species(df)
+    total_species(df)
+
+    return df
