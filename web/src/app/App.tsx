766632cf7fed8c131d9bc300b740e30dae3131ec
aathull <cf24298@bristol.ac.uk>
2026-08-11 20:11:24 +0100

    typecheck fix

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,4 +1,3 @@
-import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { SkipLink } from "../components/SkipLink";
 import { ErrorBoundary } from "./ErrorBoundary";
