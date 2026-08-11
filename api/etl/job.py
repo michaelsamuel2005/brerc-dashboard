4859830307a5c6503aa06a8b271abd4242d96b3d
Kariizi <tingting.hv@hotmail.com>
2026-08-12 00:14:31 +0100

    updated so can see logs on terminal

--- api/etl/job.py
+++ api/etl/job.py
@@ -29,6 +29,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
+    force=True, # Forces reconfiguration, ignoring prior implicit setup
 )
 
 logger = logging.getLogger(__name__)
